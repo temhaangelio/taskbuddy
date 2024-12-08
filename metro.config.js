706d524fdfb1530f7b976764e9038f5b976bbeaa
Piri Aykut <piriaykut@gmail.com>
2024-12-09 01:29:02 +0300

    dev api

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,23 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const {withNativeWind} = require('nativewind/metro');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
+// Varsayılan Metro config'i alın
 const defaultConfig = getDefaultConfig(__dirname);
 
+// SVG desteği için gerekli transformer ve resolver ayarları
 const svgTransformerConfig = {
   transformer: {
     babelTransformerPath: require.resolve('react-native-svg-transformer'),
   },
   resolver: {
+    // 'svg' uzantısını `assetExts` listesinden kaldırıyoruz ve `sourceExts` listesine ekliyoruz
     assetExts: defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
     sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
   },
 };
 
-// Merge default config with SVG transformer config
-const config = mergeConfig(defaultConfig, svgTransformerConfig);
+// Varsayılan config ile SVG desteğini birleştiriyoruz
+const mergedConfig = mergeConfig(defaultConfig, svgTransformerConfig);
 
-// Add NativeWind support
-module.exports = withNativeWind(config, {input: './global.css'});
+// NativeWind desteği ekliyoruz ve yapılandırmayı dışa aktarıyoruz
+module.exports = withNativeWind(mergedConfig, { input: './global.css' });
--- store/api.jsx
+++ store/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'https://dummyjson.com/',
+  baseURL: 'http://10.0.2.2:8090/restapi/',
   timeout: 10000,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
